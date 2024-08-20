c68979bade6ce7ec4e5c61401eebc4bfad0f4801
Juan Manuel Gomez <juan.gomez@minexus.com.ar>
2024-08-20 07:52:43 -0500

    first commit

---
+++ src/block.js
@@ -0,0 +1,23 @@
+const crypto = require('crypto');
+
+class Block {
+    constructor(
+        index,
+        timestamp,
+        data,
+        previousHash
+    ) {
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
+    }
+
+    calculateHash(){
+        return crypto.createHash('sha256').update(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).digest('hex');
+    }
+}
+
+
+module.exports = Block;
